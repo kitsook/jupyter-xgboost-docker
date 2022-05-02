f6b051c711775e4b433d48032729fe64068ba895
kitsook <clarenceho@gmail.com>
2022-05-02 06:32:02 +0000

    Extend expiration date of self-signed cert

--- entryPoint.sh
+++ entryPoint.sh
@@ -6,5 +6,4 @@ sudo -H -u ml-user -- sh -c '. /home/ml-user/.bashrc ; \
 cd ; pwd ; \
 PATH=$PATH:/home/ml-user/.local/bin:/home/ml-user:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin; 
 export PATH; nohup jupyter lab 2>&1 > /ml-disk/lab.log &'
-#bash
 sleep infinity
